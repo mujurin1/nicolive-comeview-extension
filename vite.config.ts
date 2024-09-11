cb5005ef82a9e77bb8bbc0549be0b32c1be107b5
mujurin1 <mujurin25@yahoo.co.jp>
2024-09-11 15:26:12 +0900

    v0.2.4

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import zipPack from "vite-plugin-zip-pack";
 const manifest = defineManifest({
   manifest_version: 3,
   name: "ニコ生コメビュ",
-  version: "0.2.3",
+  version: "0.2.4",
   icons: {
     128: "assets/128x128.png",
   },
