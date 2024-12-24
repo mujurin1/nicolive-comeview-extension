1de1f214f0b73af0c62c64fd2b4b434a563e9d0e
mujurin1 <mujurin25@yahoo.co.jp>
2024-12-24 14:11:20 +0900

    カラーピッカーの作成

---
+++ src/components/ColorPicker.svelte
@@ -0,0 +1,222 @@
+<script lang="ts" module>
+  type Colors = [number, number, number];
+
+  export interface ColorPickerState {
+    /** 色相:0-1 彩度:0-1 明度:0-1 */
+    hsv: Colors;
+    alpha: number;
+    /** `"#RRGGBB"`または`"#RRGGBBAA"` */
+    hex: string;
+
+    inputHex: string;
+    setFromRgbaText(hexText: string): void;
+  }
+</script>
+
+<script lang="ts">
+  import { onMount, untrack } from "svelte";
+  import ColorPickerPanel from "./ColorPickerPanel.svelte";
+
+  let {
+    hsv = $bindable([0, 0, 1]),
+    alpha = $bindable(1),
+    _hex = $bindable("#FFFFFF"),
+  }: {
+    /** 色相:0-1 彩度:0-1 明度:0-1 */
+    hsv?: Colors;
+    alpha?: number;
+    /** `"#RRGGBB"`または`"#RRGGBBAA"`. 読み取り専用(値を変更しても反映されません) */
+    _hex?: string,
+  } = $props();
+
+  let showPanel = $state(false);
+  let colorPickerButton: HTMLButtonElement;
+
+  onMount(() => {
+    document.addEventListener("mousedown", checkFocus);
+    const parent = colorPickerButton.parentElement!;
+
+    return () => {
+      document.removeEventListener("mousedown", checkFocus);
+    }
+
+    function checkFocus(e: MouseEvent) {
+      if (parent.contains(e.target as Node)) return;
+      showPanel = false;
+    }
+  });
+
+  const colorPickerState = $state<ColorPickerState>({
+    get hsv() { return hsv; },
+    set hsv(value) { hsv = value; },
+    get alpha() { return alpha; },
+    set alpha(value) { alpha = value; },
+    get hex() { return _hex; },
+    set hex(value) { _hex = value; },
+
+    get inputHex() { return inputHex; },
+    set inputHex(value) { inputHex = value; },
+    setFromRgbaText,
+  });
+
+  let inputHex = $state(getRgb());
+
+  $effect(() => {
+    const hsv = colorPickerState.hsv;
+    hsv[0]; hsv[1]; hsv[2];
+    colorPickerState.alpha;
+    untrack(() => {
+      colorPickerState.hex = inputHex = getRgb();
+    });
+  });
+
+  let hsl = $derived(getHsl());
+
+  /**
+   * 16進数のカラーコードで色をセットします
+   * @param hexText `"#RRGGBB"`または`"#RRGGBBAA"`
+   */
+  export function setFromRgbaText(hexText: string) {
+    if (!(hexText.length === 7 || hexText.length === 9)) return;
+    if (hexText[0] !== "#") return;
+    const r = parseInt(hexText.slice(1, 3), 16);
+    const g = parseInt(hexText.slice(3, 5), 16);
+    const b = parseInt(hexText.slice(5, 7), 16);
+    const a = hexText.length === 7 ? 1 : parseInt(hexText.slice(7, 9), 16) / 255;
+    if ([r, g, b, a].some(x => isNaN(x))) return;
+    colorPickerState.hsv = rgbToHsv(r,g,b);
+    colorPickerState.alpha = a;
+  }
+
+  function rgbToHsv(r: number, g: number, b: number): Colors {
+    r /= 255; g /= 255; b /= 255;
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+    const diff = max - min;
+    let h, s, v = max;
+    s = max === 0 ? 0 : diff / max;
+
+    if (max === min) {
+      h = 0;
+    } else {
+      h = r === max ? (g - b) / diff + (g < b ? 6 : 0) :
+          g === max ? (b - r) / diff + 2 :
+          (r - g) / diff + 4;
+      h /= 6;
+    }
+
+    return [h, s, v];
+  }
+
+  function getHsl(): Colors {
+    const [h, s, v] = colorPickerState.hsv;
+    const l = v * (1 - s / 2);
+    const newS = l === 0 || l === 1 ? 0 : (v - l) / Math.min(l, 1 - l);
+
+    return [h, newS, l];
+  }
+
+  function getRgb(): string {
+    const [h, s, v] = colorPickerState.hsv;
+    let r = 0, g = 0, b = 0;
+    const i = Math.floor(h * 6);
+    const f = h * 6 - i;
+    const p = v * (1 - s);
+    const q = v * (1 - f * s);
+    const t = v * (1 - (1 - f) * s);
+
+    switch (i % 6) {
+      case 0: r = v, g = t, b = p; break;
+      case 1: r = q, g = v, b = p; break;
+      case 2: r = p, g = v, b = t; break;
+      case 3: r = p, g = q, b = v; break;
+      case 4: r = t, g = p, b = v; break;
+      case 5: r = v, g = p, b = q; break;
+    }
+
+    let hex = `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    if(colorPickerState.alpha != 1) {
+      hex += toHex(colorPickerState.alpha);
+    }
+    return hex;
+  }
+
+  function toHex(n: number): string {
+    const v = Math.round(n * 255);
+    if (v <= 15) return `0${v.toString(16).toUpperCase()}`;
+    return v.toString(16).toUpperCase();
+  }
+</script>
+
+<div
+  style:--picker-panel-x={colorPickerState.hsv[1]}
+  style:--picker-panel-y={1 - colorPickerState.hsv[2]}
+  style:--picker-hue={hsl[0]}
+  style:--picker-saturation={hsl[1]}
+  style:--picker-lightness={hsl[2]}
+  style:--picker-alpha={colorPickerState.alpha}
+  class="color-picker"
+>
+  <button
+    bind:this={colorPickerButton}
+    class="color-picker-button"
+    aria-label="color-picker-switch"
+    onclick={() => {
+      showPanel = !showPanel;
+    }}
+    tabindex="-1"
+    type="button"
+  ></button>
+  {#if showPanel}
+    <div class="panel-wrap">
+      <ColorPickerPanel {colorPickerState} />
+    </div>
+  {/if}
+</div>
+
+<style>
+  :root {
+    --translate-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill-opacity=".1"><path d="M8 0h8v8H8zM0 8h8v8H0z" /></svg>');
+    --picker-height: 220px;
+    --picker-width: 200px;
+    --picker-back-color: #333;
+    --picker-slider-height: 24px;
+    --picker-ring-color: white;
+    --picker-ring-size: 28px;
+    --picker-text-size: 16px;
+    --picker-text-color: white;
+    --picker-text-back-color: #121212;
+
+    --pick-alpha: 1;
+  }
+
+  .color-picker {
+    display: inline-block;
+    position: relative;
+    user-select: none;
+
+    --picker-hsl: hsl(
+      calc(var(--picker-hue) * 360)
+      calc(var(--picker-saturation) * 100%)
+      calc(var(--picker-lightness) * 100%)
+    );
+  }
+
+  .color-picker-button {
+    box-sizing: border-box;
+    width: 32px;
+    height: 32px;
+    background-color: var(--picker-hsl);
+    margin: 1px;
+    border: 2px solid white;
+    border-radius: 2px;
+    outline: #ccc solid 1px;
+  }
+
+  .panel-wrap {
+    position: absolute;
+    bottom: 0;
+    left: 100%;
+  }
+</style>
---
+++ src/components/ColorPickerPanel.svelte
@@ -0,0 +1,196 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { handleElementInteraction } from "../lib/DomEvent";
+  import type { ColorPickerState } from "./ColorPicker.svelte";
+
+  let { colorPickerState }: { colorPickerState: ColorPickerState } = $props(); 
+
+  let pickerPanel: HTMLDivElement;
+  let hueSlider: HTMLDivElement;
+  let alphaSlider: HTMLDivElement;
+
+  onMount(() => {
+    const disposes = [
+      handleElementInteraction(pickerPanel, {
+        start: setSaturationAndLightness,
+        mouseMove: setSaturationAndLightness,
+      }),
+      handleElementInteraction(hueSlider, {
+        start: setHue,
+        mouseMove: setHue,
+      }),
+      handleElementInteraction(alphaSlider, {
+        start: setAlpha,
+        mouseMove: setAlpha,
+      }),
+    ];
+
+    return () => {
+      for (const dispose of disposes) dispose();
+    };
+
+    function setSaturationAndLightness(e: MouseEvent) {
+      if (e.target == null) return;
+      const { x, y } = pickerPanel.getBoundingClientRect();
+      colorPickerState.hsv[1] = Math.max(0, Math.min(1, (e.clientX - x) / pickerPanel.clientWidth));
+      colorPickerState.hsv[2] = 1 - Math.max(0, Math.min(1, (e.clientY - y) / pickerPanel.clientHeight));
+    }
+    function setHue(e: MouseEvent) {
+      if (e.target == null) return;
+      const { x } = hueSlider.getBoundingClientRect();
+      colorPickerState.hsv[0] = Math.max(0, Math.min(1, (e.clientX - x) / hueSlider.clientWidth));
+    }
+    function setAlpha(e: MouseEvent) {
+      if (e.target == null) return;
+      const { x } = alphaSlider.getBoundingClientRect();
+      colorPickerState.alpha = Math.max(0, Math.min(1, (e.clientX - x) / alphaSlider.clientWidth));
+    }
+  });
+</script>
+
+<div class="color-picker-panel">
+  <div class="picker-panel-wrap">
+    <div bind:this={pickerPanel} class="picker-panel">
+      {@render Pick()}
+    </div>
+  </div>
+  <div class="picker-dummy"></div>
+  <div bind:this={hueSlider} class="hue-slider">
+    {@render Pick()}
+  </div>
+  <div bind:this={alphaSlider} class="alpha-slider">
+    <div class="alpha-slider-bg">
+      {@render Pick()}
+    </div>
+  </div>
+  <div class="input-area">
+    <input
+      class="hex-input"
+      oninput={() => colorPickerState.setFromRgbaText(colorPickerState.inputHex)}
+      type="text"
+      bind:value={colorPickerState.inputHex}
+    />
+  </div>
+</div>
+
+{#snippet Pick()}
+  <div class="picker-pick" tabindex="-1">
+    <div class="pick-color"></div>
+  </div>
+{/snippet}
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  .hex-input {
+    width: 100%;
+    height: 100%;
+    border: none;
+    font-size: var(--picker-text-size);
+    padding: 0 4px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .color-picker-panel {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--picker-back-color);
+    height: var(--picker-height);
+    width: var(--picker-width);
+    padding: 6px;
+    border-radius: 8px;
+  }
+
+  .picker-panel-wrap {
+    flex: 1 1 0;
+    padding-bottom: 12px;
+    background-color: black;
+    border-radius: 8px 8px 0 0;
+
+    .picker-panel {
+      height: 100%;
+      background-image: linear-gradient(0deg, #000, #0000), linear-gradient(90deg, #fff, #0000);
+      background-color: hsl(calc(var(--picker-hue) * 360) 100% 50%);
+      border-radius: inherit;
+
+      .picker-pick {
+        --pick-top: calc(var(--picker-panel-y) * 100%);
+        --pick-left: calc(var(--picker-panel-x) * 100%);
+        --pick-color: var(--picker-hsl);
+      }
+    }
+  }
+
+  .hue-slider {
+    flex: 0 0 auto;
+    height: var(--picker-slider-height);
+    background: linear-gradient(
+      90deg, red 0, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, red
+    );
+
+    .picker-pick {
+      --pick-top: 50%;
+      --pick-left: calc(var(--picker-hue) * 100%);
+      --pick-color: hsl(calc(var(--picker-hue) * 360) 100% 50%);
+    }
+  }
+
+  .alpha-slider {
+    flex: 0 0 auto;
+    height: var(--picker-slider-height);
+    background-color: white;
+    background-image: var(--translate-image);
+
+    .alpha-slider-bg {
+      width: 100%;
+      height: 100%;
+      background-image: linear-gradient(90deg, #0000, var(--picker-hsl));
+
+      .picker-pick {
+        --pick-top: 50%;
+        --pick-left: calc(var(--picker-alpha) * 100%);
+        --pick-color: var(--picker-hsl);
+        --pick-alpha: var(--picker-alpha)
+      }
+    }
+  }
+  .input-area {
+    flex: 0 0 auto;
+    height: var(--picker-slider-height);
+  }
+
+  .picker-pick {
+    position: relative;
+    width: var(--picker-ring-size);
+    height: var(--picker-ring-size);
+    transform: translate(-50%, -50%);
+    border: 3px solid var(--picker-ring-color);
+    border-radius: 50%;
+
+    top: var(--pick-top);
+    left: var(--pick-left);
+    background-color: white;
+    background-image: var(--translate-image);
+
+    .pick-color {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: inherit;
+      background-color: var(--pick-color);
+      opacity: var(--pick-alpha);
+    }
+
+    /* 親がフォーカスされたらこの要素にフォーカスすれば完璧 */
+    &:focus-within,
+    &:hover {
+      outline: none;
+      transform: translate(-50%, -50%) scale(1.1);
+    }
+  }
+</style>
